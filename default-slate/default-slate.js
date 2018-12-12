b7a3c2a46a8f246e948611efbd25e27c7a6f4d8d
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2018-12-12 22:23:13 +0000

    updated to bootstrap4

--- default-slate/default-slate.js
+++ default-slate/default-slate.js
@@ -1,1 +1,5 @@
-require(["/themes/default/default.js"]);
+require(["/themes/default/default.js"], function () {
+    require(["//cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"], function () {
+        $("nav#profileSections li").addClass("border-0");
+    });
+});
--- default/default.js
+++ default/default.js
@@ -1,16 +1,21 @@
-require(["//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js"], function () {
-	require(["//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"], function () {
-		$("body").show().wrapInner("<div class='container'><div class='row'><div class='col-md-9'></div></div></div>");
-		$("h1").addClass("page-header");
-		$("body>.container>.row").prepend("<div class='col-md-3 hidden-xs hidden-sm'></div>");
-		$("body>.container>.row>.col-md-3").append($("nav"));
-		$("nav#profileSections").prepend($("#picture"));
-		$("nav#profileSections ul").addClass("nav nav-pills nav-stacked");
-        $("#picture").addClass("img-circle").wrap("<a href='#'></a>");
+require(["//cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js", "//cdnjs.cloudflare.com/ajax/libs/stickyfill/2.1.0/stickyfill.min.js"], function () {
+    require(["//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"], function () {
+        $("body").show().wrapInner("<div class='container'><div class='row'><div class='col-md-9'></div></div></div>");
+        $("body>.container>.row").prepend("<div class='col-md-3 d-sm-none d-md-block'></div>");
+        $("body>.container>.row>.col-md-3").append($("nav"));
+        $("nav#profileSections").prepend($("#picture"));
+        $("nav#profileSections ul").addClass("nav nav-pills flex-column");
+        $("nav#profileSections li").addClass("border-bottom");
+        $("nav#profileSections a").addClass("nav-link");
+        $("#picture").addClass("img-fluid rounded-circle").wrap("<a href='#'></a>");
 
-        $("#summary").addClass("panel panel-info").wrapInner("<div class='panel-body bg-info'></div>");
+        if (!$("#summary").length) {$(".col-md-9").prepend("<section id='summary' />")}
+        $("#summary").addClass("jumbotron mt-4 p-4 mb-0");
+        $("#summary:not(:empty)").prepend("<hr class='my-4'>");
+        $("#summary").prepend($("h2").addClass("lead"));
+        $("#summary").prepend($("h1").addClass("display-3"));
 
-		$("ul.highlights,ul.keywords,ul.courses").addClass("list-inline");
+        $("ul.highlights,ul.keywords,ul.courses").addClass("list-inline");
         $("ul.highlights li,ul.keywords li,ul.courses li").addClass("badge");
         $("ul.highlights").prepend("<li style='padding-right: 0'><i class='fa fa-tags'></i></li>");
         $("ul.courses").prepend("<li style='padding-right: 0'><i class='fa fa-graduation-cap'></i></li>");
@@ -44,22 +49,22 @@ require(["//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js"], functi
         $("#education .area").prepend("<i class='fa fa-tags'></i>&nbsp;");
 
         $("#profiles").addClass("list-unstyled");
-		$("#profiles a").each(function () {
-			$(this).prepend("<i class='fa fa-" + $(this).prop("class") + "'></i>&nbsp;");
+        $("#profiles a").each(function () {
+            $(this).prepend("<i class='fa fa-" + $(this).prop("class") + "'></i>&nbsp;");
         });
 
-		$("#work>ul, #honorsAwards>ul, #education>ul, #publications>ul, #certifications>ul, #interests>ul, #volunteer>ul, #references>ul, #skills>ul, #awards>ul").addClass("list-group");
-		$("#work>ul>li, #honorsAwards>ul>li, #education>ul>li, #publications>ul>li, #certifications>ul>li, #interests>ul>li, #volunteer>ul>li, #references>ul>li, #skills>ul>li, #awards>ul>li").addClass("list-group-item");
+        $("#work>ul, #honorsAwards>ul, #education>ul, #publications>ul, #certifications>ul, #interests>ul, #volunteer>ul, #references>ul, #skills>ul, #awards>ul").addClass("list-group");
+        $("#work>ul>li, #honorsAwards>ul>li, #education>ul>li, #publications>ul>li, #certifications>ul>li, #interests>ul>li, #volunteer>ul>li, #references>ul>li, #skills>ul>li, #awards>ul>li").addClass("list-group-item border-right-0 border-left-0");
 
-		$("#work>ul>li, #volunteer>ul>li, #education>ul>li").prepend("<div class='header clearfix'></div>");
+        $("#work>ul>li, #volunteer>ul>li, #education>ul>li").prepend("<div class='header clearfix'></div>");
         $("#work>ul>li").each(function () {
-        	var position = $(this).find(".position");
-        	var company = $(this).find(".company")
+            var position = $(this).find(".position");
+            var company = $(this).find(".company")
             if (position.length) $(this).find(".header").append(position);
             if (company.length) $(this).find(".header").append(company);
             if (position.length && company.length) {
                 position.append("&nbsp;-&nbsp;");
-			}
+            }
         });
         $("#volunteer>ul>li").each(function () {
             var position = $(this).find(".position");
@@ -83,8 +88,9 @@ require(["//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js"], functi
 
         $("ul.list-group li.list-group-item>h4").addClass("list-group-item-heading");
         $("ul.list-group li.list-group-item div.summary").addClass("list-group-item-text");
+        $("ul.list-group li:last-child").addClass("border-bottom-0");
 
-        $("#languages ul").addClass("list-unstyled");
+        $("#languages ul").addClass("list-unstyled card-body");
         $("#languages .fluency").addClass("progress-bar").attr("aria-valuemin", "0").attr("aria-valuemax", "5");
         $("#languages .fluency").wrap("<div class='progress'></div>");
         $("#languages .fluency-elementary").css("width", "20%");
@@ -104,44 +110,42 @@ require(["//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js"], functi
             $("#skills .level-4").html('<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i>');
             $("#skills .level-5").html('<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>');
         } else {
-            //$("#skills>ul").removeClass("list-group").addClass("list-inline");
             var skills = [];
             $("#skills li").each (function () {skills.push($(this).text().trim())});
-            $("#skills>ul").replaceWith("<div class='panel-body'>" + skills.join(", ") + "</div>");
+            $("#skills>ul").replaceWith("<div class='card-body'>" + skills.join(", ") + "</div>");
         }
 
-		$("section:not('#summary')").addClass("panel panel-primary").prepend("<div class='panel-heading'></div>");
-		$('#basics .panel-heading').after("<div class='panel-body'></div>");
-        $("section:not(#basics)>div, section:not(#basics)>ul").addClass("panel-body");
-        $("#basics .panel-body").nextAll().appendTo("#basics .panel-body");
+        $("section:not('#summary')").addClass("card mt-4");
+        $("section h3").addClass("card-header");
+        $('#basics .card-header').after("<div class='card-body'></div>");
+        $("section:not(#basics)>div").addClass("card-body");
+        $("#basics .card-body").nextAll().appendTo("#basics .card-body");
 
-		$("section").each(function () {
-		    $(this).find(".panel-heading").append($(this).find("h3"));
-		});
-
-		$("#basics .panel-body").append("<div id='container'><div class='row'><div class='col-sm-6 leftcol'></div><div class='col-sm-6 rightcol'></div></div></div>")
+        $("#basics .card-body").append("<div id='container'><div class='row'><div class='col-sm-6 leftcol'></div><div class='col-sm-6 rightcol'></div></div></div>")
         $("#basics .leftcol").append($("#phone"));
-		$("#basics .leftcol").append($("#location"));
+        $("#basics .leftcol").append($("#location"));
         $("#email").prependTo("#profiles");
         $("#website").prependTo("#profiles");
-		$("#basics .rightcol").append($("#profiles"));
-		$("#website").replaceWith("<li id='website'>" + $("#website").html() + "</li>");
+        $("#basics .rightcol").append($("#profiles"));
+        $("#website").replaceWith("<li id='website'>" + $("#website").html() + "</li>");
         $("#email").replaceWith("<li id='email'>" + $("#email").html() + "</li>");
 
-		$("#designer")
-			.attr("href", "http://velocv.com")
-			.text("VeloCV");
+        $("footer").addClass("mt-4 text-center");
+        $("#designer")
+            .attr("href", "http://velocv.com")
+            .text("VeloCV");
 
-		if ($("nav#profileLanguages").length) {
-			var selected_language = $("nav#profileLanguages li.active").text();
-			$("nav#profileLanguages li.active").remove();
-			$("h1").append('<div class="btn-group pull-right" id="profileLanguagesSelector"><button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">' +  selected_language + ' <span class="caret"></span></button><ul class="dropdown-menu" role="menu"></ul></div>');
-			$("#profileLanguagesSelector ul").append($("nav#profileLanguages li"));
-			$("nav#profileLanguages").remove();
-		}
+        if ($("nav#profileLanguages").length) {
+            var selected_language = $("nav#profileLanguages li.active").text();
+            $("nav#profileLanguages li.active").remove();
+            $("#summary").prepend('<div class="btn-group pull-right" id="profileLanguagesSelector"><button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' +  selected_language + ' </button><div class="dropdown-menu"></div></div>');
+            $("#profileLanguagesSelector .dropdown-menu").append($("nav#profileLanguages li a"));
+            $("#profileLanguagesSelector .dropdown-menu a").addClass("dropdown-item");
+            $("nav#profileLanguages").remove();
+        }
 
         $(".fa").addClass("fw");
-		$("nav#profileSections").affix();
-		$('body').scrollspy({target:'nav'});
-	});
+        Stickyfill.add($("nav#profileSections"));
+        $('body').scrollspy({target:'nav'});
+    });
 });
